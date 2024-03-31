2abba351364c0fcb6d6e2e71e70b9e803dad5b1d
ChMueedZafar <chmueedzafar@gmail.com>
2024-04-01 00:39:53 +0500

    First Question

---
+++ script.js
@@ -0,0 +1,12 @@
+let p1 = prompt("Entre the First number")
+let p2 = prompt("Entre the Second number")
+
+if (p1>p2){
+    console.log('${p1} is greater then ${p2}');
+}
+else if (p1<p2){
+    console.log('$ {p1}isgreater then ${p2}');
+}
+else{
+    alert('${p1}is equal to ${p2}');
+}
