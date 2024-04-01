49cb443622077d5e86312ff2ff2ce6ba023e97dc
ChMueedZafar <chmueedzafar@gmail.com>
2024-04-01 22:41:42 +0500

    second Question

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Question no 1
 let p1 = prompt("Entre the First number")
 let p2 = prompt("Entre the Second number")
 
 if (p1>p2){
-    console.log('${p1} is greater then ${p2}');
+    console.log("${p1} is greater then ${p2}");
 }
 else if (p1<p2){
     console.log('$ {p1}isgreater then ${p2}');
 }
 else{
     alert('${p1}is equal to ${p2}');
+}
+// Question 2
+let price1 = prompt("entre the first number")
+let price2 =prompt("entre the second number")
+let price3 =prompt("entre the third number")
+
+if (price1>price2&&price3){
+    console.log("${p1}is greater then {p2}&&{p3}");
 }
